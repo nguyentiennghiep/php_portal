812a72fdedfd2633103079296f7697a6971938a1
nguyentiennghiep <nguyen_tien_nghiep@mediado.jp>
2018-12-14 12:04:43 +0900

    add template

---
+++ templates/application-page.php
@@ -0,0 +1,106 @@
+<?php 
+	/* 
+	Template Name: Application
+	*/
+?>
+
+<?php get_header(); ?>
+<div class="container">
+	<div id="appplication-page-table">
+		<h1>各種申請</h1>
+	   	<table>
+		    <tbody>
+		        <tr>
+		            <td rowspan="3" width="200">メーリングリスト</td>
+		            <td width="400">作成</td>
+		            <td><a href="https://news.mediado.info/?page_id=3042">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">削除</td>
+		            <td><a href="https://news.mediado.info/?page_id=3040"> 申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">メンバー追加/削除</td>
+		            <td><a href="https://news.mediado.info/?page_id=3038">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td rowspan="2" width="200">共有フォルダ</td>
+		            <td width="400">作成</td>
+		            <td><a href="https://news.mediado.info/?page_id=3044">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">権限付与/削除</td>
+		            <td><a href="https://news.mediado.info/?page_id=3046">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td rowspan="6" width="200">PC</td>
+		            <td width="400">見積もり依頼</td>
+		            <td><a href=" https://news.mediado.info/?page_id=3028 ">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">購入依頼</td>
+		            <td><a href="https://news.mediado.info/wp-content/uploads/2018/01/pc_orderform.xlsx">申請書ダウンロード</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">回収・廃棄</td>
+		            <td><a href="https://news.mediado.info/?page_id=3024">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">障害対応</td>
+		            <td><a href="https://news.mediado.info/?page_id=3030">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">セットアップ</td>
+		            <td><a href="https://news.mediado.info/?page_id=3022">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">在庫機貸し出し</td>
+		            <td><a href="https://news.mediado.info/?page_id=3026">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="200">新社員用PC</td>
+		            <td width="400">在庫機割り当て</td>
+		            <td><a href="https://news.mediado.info/?page_id=3922">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="200">FTP</td>
+		            <td width="400">アカウント作成</td>
+		            <td><a href="https://news.mediado.info/?page_id=2994">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td rowspan="3" width="200">SFTP</td>
+		            <td width="400"> アカウント作成</td>
+		            <td><a href="https://news.mediado.info/?page_id=3907">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">接続元IPアドレス追加</td>
+		            <td><a href=" https://news.mediado.info/?page_id=3912"> 申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">接続元IPアドレス変更</td>
+		            <td><a href=" https://news.mediado.info/?page_id=3943"> 申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="200">VPN</td>
+		            <td width="400">アカウント作成</td>
+		            <td><a href="https://news.mediado.info/?page_id=3036">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td rowspan="2" width="200">Redmine</td>
+		            <td width="400">プロジェクト作成</td>
+		            <td><a href="https://news.mediado.info/?page_id=3034">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td width="400">アカウント作成</td>
+		            <td><a href="https://news.mediado.info/?page_id=3032">申請フォーム</a></td>
+		        </tr>
+		        <tr>
+		            <td rowspan="2" width="200">GitBucket</td>
+		            <td width="400">ユーザ追加/削除</td>
+		            <td><a href="https://news.mediado.info/?page_id=2996">申請フォーム</a></td>
+		        </tr>
+	    	</tbody>
+	 	</table>
+	</div>
+</div>
+<?php get_footer(); ?>
---
+++ templates/download-page.php
@@ -0,0 +1,75 @@
+<?php 
+	/* 
+	Template Name: Download
+	*/
+?>
+
+<?php get_header(); ?>
+<div class="container">
+	<div id="appplication-page-table">
+		<h1>マニュアル・ドライバダウンロード</h1>
+	   	<table>
+		    <tbody>
+		        <tr>
+		            <td colspan="2">Skysea Client View インストール手順書</td>
+		            <td> 作成中</td>
+		        </tr>
+		        <tr>
+		            <td rowspan="3">VPNアカウント設定手順書</td>
+		            <td>Windows 7版</td>
+		            <td> <a href="https://news.mediado.info/wp-content/uploads/2017/12/VPN_setting_manual_win7.pdf" target="_black"><img
+		                        class="alignleft" src="https://news.mediado.info/wp-content/uploads/2017/12/Download-PDF-Button.png"
+		                        width="131" height="35" /></a></td>
+		        </tr>
+		        <tr>
+		            <td> Windows 10版</td>
+		            <td> <a href="https://news.mediado.info/wp-content/uploads/2017/12/VPN_setting_manual_win10.pdf" target="_black"><img
+		                        class="alignleft" src="https://news.mediado.info/wp-content/uploads/2017/12/Download-PDF-Button.png"
+		                        width="131" height="35" /></a></td>
+		        </tr>
+		        <tr>
+		            <td> Mac版</td>
+		            <td> <a href="https://news.mediado.info/wp-content/uploads/2017/12/VPN_setting_manual_mac.pdf" target="_black"><img
+		                        class="alignleft" src="https://news.mediado.info/wp-content/uploads/2017/12/Download-PDF-Button.png"
+		                        width="131" height="35" /></a></td>
+		        </tr>
+		        <tr>
+		            <td rowspan="4">プリンタードライバー/マニュアルのインストール</td>
+		            <td>Windows版(5階)</td>
+		            <td> <a href="https://news.mediado.info/wp-content/uploads/2018/03/windowsプリンタードライバー(5F).zip" target="_black"><img
+		                        class="alignleft" src="https://news.mediado.info/wp-content/uploads/2018/03/VectorButtons.jpg"
+		                        width="131" height="35" /></a></td>
+		        </tr>
+		        <tr>
+		            <td>Windows版(8階)</td>
+		            <td> <a href="https://news.mediado.info/wp-content/uploads/2018/03/windowsプリンタードライバー(8F).zip" target="_black"><img
+		                        class="alignleft" src="https://news.mediado.info/wp-content/uploads/2018/03/VectorButtons.jpg"
+		                        width="131" height="35" /></a></td>
+		        </tr>
+		        <tr>
+		            <td>Mac版(5階)</td>
+		            <td> <a href="https://news.mediado.info/wp-content/uploads/2018/09/17e917431cb282789bdb8cfd000d8eb4-1.zip"
+		                    target="_black"><img class="alignleft" src="https://news.mediado.info/wp-content/uploads/2018/03/VectorButtons.jpg"
+		                        width="131" height="35" /></a></td>
+		        </tr>
+		        <tr>
+		            <td>Mac版(8階)</td>
+		            <td> <a href="https://news.mediado.info/wp-content/uploads/2018/03/Macプリンターマニュアル.zip" target="_black"><img
+		                        class="alignleft" src="https://news.mediado.info/wp-content/uploads/2018/03/VectorButtons.jpg"
+		                        width="131" height="35" /></a></td>
+		        </tr>
+		    </tbody>
+		</table>
+		<table>
+		    <tbody>
+		        <tr>
+		            <td>メーリスメンバー追加・削除・マネージャー権限付与手順 手順</td>
+		            <td> <a href="https://news.mediado.info/wp-content/uploads/2018/07/6fb5a5bcb543c8f6b7aca191526d30c6.pdf"
+		                    target="_black"><img class="alignleft" src="https://news.mediado.info/wp-content/uploads/2017/12/Download-PDF-Button.png"
+		                        width="131" height="35" /></a></td>
+		        </tr>
+		    </tbody>
+		</table>
+	</div>
+</div>
+<?php get_footer(); ?>
